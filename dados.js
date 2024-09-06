6f07189df6f2dbef8ff97f3c106b8ce99db9ff4b
zksidney <165915321+zksidney@users.noreply.github.com>
2024-09-06 19:53:50 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,21 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal", 
+        descricao: "Raissa Leal é uma skatista profissional brasileira, conhecida por sua habilidade e carisma. Aos 13 anos, tornou-se a mais jovem medalhista olímpica da história do Brasil.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8"
+    },
+    {
+        titulo: "Rebeca Andrade", 
+        descricao: "Rebeca Andrade é, sem dúvida, um dos maiores nomes da ginástica artística brasileira. Com um talento inegável e uma força de vontade admirável, ela conquistou o coração dos brasileiros e se tornou um ícone do esporte nacional.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginastica ginástica ouro"
+    },
+    {
+        titulo: "Beatriz Sousa", 
+        descricao: "Beatriz Souza é uma judoca brasileira, conhecida por sua força e técnica. Conquistou a medalha de ouro nos Jogos Olímpicos de 2024, tornando-se um dos principais nomes do judô brasileiro.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza_(judoca)",
+        tags: "judo judoca judô judoka"
+    }
+];
+
